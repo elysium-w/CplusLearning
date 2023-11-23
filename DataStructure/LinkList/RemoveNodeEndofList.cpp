ff652b1b9d4aa31ed8522971042d474d5d4e54c7
WYA1110 <wya10241110@163.com>
2023-11-23 23:51:54 +0800

    second

--- DataStructure/LinkList/RemoveNodeEndofList.cpp
+++ DataStructure/LinkList/RemoveNodeEndofList.cpp
@@ -4,5 +4,18 @@
 //给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
 #include "func.h"
 ListNode* removeNthFromEnd(ListNode* head, int n) {
-
+    ListNode* dummyHead=new ListNode(0);
+    dummyHead->next=head;
+    ListNode* fast=dummyHead;
+    ListNode* slow=dummyHead;
+    while (n--&&fast!= nullptr){
+        fast=fast->next;
+    }
+    fast=fast->next;
+    while (fast!= nullptr){
+        slow=slow->next;
+        fast=fast->next;
+    }
+    slow->next=slow->next->next;
+    return dummyHead->next;
 }
