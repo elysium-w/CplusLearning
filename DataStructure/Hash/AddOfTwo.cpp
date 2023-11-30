6ac3d809da64bd2123aa51b51f3c53b3b9120b93
WYA1110 <wya10241110@163.com>
2023-11-30 22:11:59 +0800

    hashtable

---
+++ DataStructure/Hash/AddOfTwo.cpp
@@ -0,0 +1,43 @@
+//
+// Created by illumwang on 11/30/23.
+//
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+
+你可以按任意顺序返回答案。
+
+
+
+示例 1：
+
+输入：nums = [2,7,11,15], target = 9
+输出：[0,1]
+解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+示例 2：
+
+输入：nums = [3,2,4], target = 6
+输出：[1,2]
+示例 3：
+
+输入：nums = [3,3], target = 6
+输出：[0,1]
+
+ */
+using namespace std;
+#include <unordered_map>
+#include <unordered_set>
+#include "vector"
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int,int> res;
+    for (int i = 0; i < nums.size(); ++i) {
+        int tmp=target-nums[i];
+        auto iter=res.find(tmp);
+        if (iter!=res.end()){
+            return {iter->second,i};
+        }
+        res.insert(pair<int, int>(nums[i], i));
+    }
+    return {};
+}
--- DataStructure/Hash/funcAndStruct.h
+++ DataStructure/Hash/funcAndStruct.h
@@ -6,7 +6,9 @@
 #define CPLUSLEARNING_FUNCANDSTRUCT_H
 
 #include <string>
+#include <vector>
 
 using namespace std;
 bool isAnagram(string s, string t);
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2);
 #endif //CPLUSLEARNING_FUNCANDSTRUCT_H
